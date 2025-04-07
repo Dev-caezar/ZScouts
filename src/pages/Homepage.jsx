81a279e9885223f38216d4177d6a22dbebc27c56
Caezar-coder <okochristian6@gmail.com>
2025-04-07 16:20:30 +0100

    removed some components

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "../styles/home.css";
-import PricingPlans from "../components/PricingPlan";
-import Testimonials from "../components/Testimonials";
 import Hero from "../components/Hero";
 
 const Homepage = () => {
   return (
     <div className="home_body">
       <Hero />
-      <PricingPlans />
-      <Testimonials />
     </div>
   );
 };
