c241d9c6939b7187fcfe39fcfe906a1c00a9f9d0
Caezar-coder <okochristian6@gmail.com>
2025-04-11 13:14:11 +0100

    routing

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -19,91 +19,85 @@ import PlayerProfile from "../pages/playerProfile";
 // import RegisterPlayer from "../auth/RegisterPlayer";
 import EmailVerify from "../auth/emailVerify";
 
-
-
-
-
 export const Element = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomeLayout />,
-        children: ([
-            {
-                index: true,
-                element: <Homepage />
-            },
-            {
-                path: "about_us",
-                element: <AboutUs />
-            },
-            {
-                path: "contact_us",
-                element: <ContactUs />
-            },
-        ]),
-    },
-    {
-        element: <AuthLayout />,
-        children: ([
-            {
-                path: "/player_login",
-                element: <PlayerLogin />
-            },
-            {
-                path: "/scout_login",
-                element: <ScoutLogin />
-            },
-            // {
-            //     path: "/player_register",
-            //     element: <RegisterPlayer />
-            // },
-            {
-                path: "/scout_register",
-                element: <ScoutRegister />
-            },
-            {
-                path: "/email_page",
-                element: <EmailPage />
-            },
-            {
-                path: "/email_verify",
-                element: <EmailVerify />
-            },
-            {
-                path: "/email_notify",
-                element: <EmailNotify />
-            },
-            {
-                path: "/forgot_password",
-                element: <ForgotPassword />
-            },
-            {
-                path: "/reset_password",
-                element: <ResetPassword />
-            },
-            {
-                path: "/login_option",
-                element: <LoginOption />
-            },
-            {
-                path: "/signup_option",
-                element: <SignupOption />
-            },
-            
-        ])
-    },
-    {
-        element: <DashboardLayout />,
-        children: ([
-            {
-                path: "player_profile",
-                element:<PlayerProfile />
-            }
-        ])
-    },
-
-    {
-        path: "*",
-        element: <ErrorPage />
-    },
-])
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "about_us",
+        element: <AboutUs />,
+      },
+      {
+        path: "contact_us",
+        element: <ContactUs />,
+      },
+    ],
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/player_login",
+        element: <PlayerLogin />,
+      },
+      {
+        path: "/scout_login",
+        element: <ScoutLogin />,
+      },
+      // {
+      //     path: "/player_register",
+      //     element: <RegisterPlayer />
+      // },
+      {
+        path: "/scout_register",
+        element: <ScoutRegister />,
+      },
+      {
+        path: "/email_page",
+        element: <EmailPage />,
+      },
+      {
+        path: "/email_verify",
+        element: <EmailVerify />,
+      },
+      {
+        path: "/email_notify",
+        element: <EmailNotify />,
+      },
+      {
+        path: "/forgot_password",
+        element: <ForgotPassword />,
+      },
+      {
+        path: "/reset_password",
+        element: <ResetPassword />,
+      },
+      {
+        path: "/login_option",
+        element: <LoginOption />,
+      },
+      {
+        path: "/signup_option",
+        element: <SignupOption />,
+      },
+    ],
+  },
+  {
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "player_profile",
+        element: <PlayerProfile />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
